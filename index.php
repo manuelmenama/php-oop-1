e30f0f622479a569d4088b2bb4f05204f14ae50a
manuelmenama <manuel.mena090589@gmail.com>
2022-12-07 15:53:13 +0100

    added poster link

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
   require_once './class/Movie.php';
   $non_si_sevizia_un_paperino = new Movie("Non Si Sevizia Un Paperino", "102 minuti", "1972", "Lucio Fulci",["giallo","thriller"]);
-  $milano_odia = new Movie("Milano Odia: La Polizia Non Può Sparare","100 minuti","1974","Umberto Lenzi",["noir", "poliziesco", "thriller"])
+  $milano_odia = new Movie("Milano Odia: La Polizia Non Può Sparare","100 minuti","1974","Umberto Lenzi",["noir", "poliziesco", "thriller"]);
+
+  $non_si_sevizia_un_paperino->takePoster("https://bmoviezone.files.wordpress.com/2011/01/nonsiseviziaunpaperino.jpg");
+  $milano_odia->takePoster("https://bmoviezone.files.wordpress.com/2011/01/milanoodia-lapolizia-nonpuo-sparare.jpg");
+
+  var_dump($non_si_sevizia_un_paperino);
+  var_dump($milano_odia);
 ?>
 
 <!DOCTYPE html>
@@ -13,6 +19,8 @@
   <title>Never lose h-oop-e</title>
 </head>
 <body>
+
+  
   
 </body>
 </html>
